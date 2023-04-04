27c883361358bf02ff9614d3b7f9489ea7279a84
rayan1159 <baskruithof999@gmail.com>
2023-04-04 12:33:34 +0100

    Updated landing page

--- site/src/app/user/landing/landing.component.ts
+++ site/src/app/user/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-landing',
+  selector: 'app-root',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.less']
 })
