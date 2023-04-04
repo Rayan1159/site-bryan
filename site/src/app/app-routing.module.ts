8e89b8cf6c427186ed4e8dfe6c6147618cfb91ae
rayan1159 <baskruithof999@gmail.com>
2023-04-04 11:32:36 +0100

    Started working on landing design

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingComponent} from "./user/landing/landing.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LandingComponent
+  }
+];
+
+routes.push({
+  path: '',
+  redirectTo: '/landing',
+  pathMatch: 'full'
+});
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
